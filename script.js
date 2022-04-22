4ac15f45a7b3a502872ed055b9afe71d59ef6f48
vxcgh123 <68682960+vxcgh123@users.noreply.github.com>
2022-04-22 13:21:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector("#login-Form");
 const loginInput = document.querySelector("#login-Form input");
-
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -8,4 +8,9 @@ function onLoginSubmit(event) {
   console.log(username);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit); 
+link.addEventListener("click", handleLinkClick);
